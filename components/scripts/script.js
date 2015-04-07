3b15f0b453325c1e28b9d304ec41aff669682b74
Diego de Souza <diegotrus7@gmail.com>
2015-04-06 22:30:11 -0400

    Basic CoffeeScript syntax

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,3 +1,51 @@
+var add, c, data, name;
+
 $(function() {
-  return $('h1').append(' Hello' + ' World');
+  return 'h1'.append('Hello World');
 });
+
+console.log("Well Well");
+
+c = 4;
+
+name = 'Diego';
+
+add = function(a, b) {
+  var d;
+  d = 3;
+  return a + b;
+};
+
+data = {
+  name: "Diego de Souza",
+  numbers: [1, 2, 3, 4],
+  links: {
+    twitter: "http//twitter.com/diegotrus7",
+    google: "http//google.com"
+  }
+};
+
+function add(a, b) {
+  return a+b
+};
+
+
+/*
+Comments are shown in code block
+ */
+
+
+/* 
+Syntax Overview
+
+•No semicolons
+•No curly braces
+•Don't need the var keyword
+•Variables scoped automatically
+•No return statement
+•Parentheses often optional 
+•No function keyword
+•Tripple # for comments to show
+•single # for inline comment
+•` for javascript code (tick mark)
+ */
