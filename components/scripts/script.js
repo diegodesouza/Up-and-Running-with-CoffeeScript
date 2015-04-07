cffca6849fe4ec324354c6f15f6523ff3c5ae100
Diego de Souza <diegotrus7@gmail.com>
2015-04-06 22:43:10 -0400

    Using realistic assets

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,51 +1,5 @@
-var add, c, data, name;
+var addStuff;
 
-$(function() {
-  return 'h1'.append('Hello World');
-});
-
-console.log("Well Well");
-
-c = 4;
-
-name = 'Diego';
-
-add = function(a, b) {
-  var d;
-  d = 3;
-  return a + b;
-};
-
-data = {
-  name: "Diego de Souza",
-  numbers: [1, 2, 3, 4],
-  links: {
-    twitter: "http//twitter.com/diegotrus7",
-    google: "http//google.com"
-  }
-};
-
-function add(a, b) {
-  return a+b
-};
-
-
-/*
-Comments are shown in code block
- */
-
-
-/* 
-Syntax Overview
-
-•No semicolons
-•No curly braces
-•Don't need the var keyword
-•Variables scoped automatically
-•No return statement
-•Parentheses often optional 
-•No function keyword
-•Tripple # for comments to show
-•single # for inline comment
-•` for javascript code (tick mark)
- */
+(addStuff = function() {
+  return $('#content').append("Don't miss it!");
+})();
